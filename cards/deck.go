01ceccddf01857feaf21af93c46fb4dde258eca7
Daniela Morales <58857379+daniela2001-png@users.noreply.github.com>
2024-10-04 04:29:13 +0000

    [Add] new deck methods

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -29,4 +33,22 @@ func newDeck() deck {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(data), ",")
+	return deck(s)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	cards := newDeck()
 	handDeck, remainingDeck := deal(cards, 5)
 	handDeck.print()
 	remainingDeck.print()
+	myString := cards.toString()
+	fmt.Println(myString)
+	cards.saveToFile("my_cards")
+	newCards := newDeckFromFile("my_cards")
+	newCards.print()
 }
